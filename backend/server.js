7ec71bb597857b9cf0c0bace15e2a6436b1de3a2
sumitlokhande <sumitlokhande858@gmail.com>
2026-06-10 16:37:36 +0530

    cors error fix update

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import authRoutes from "./routes/authRoute.js";
 const app = express();
 
 const allowedOrigins = [
-  "https://game-verse-pern-stack.vercel.app/",
+  "https://game-verse-pern-stack.vercel.app/api",
   "http://localhost:5173",
 ];
 console.log("Allowed Origins:", allowedOrigins);
